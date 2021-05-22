d10a90d09a761d850626bdcba83285f83fb8c53c
Grizvel32 <grizenkov1996@gmail.com>
2021-05-22 11:08:42 +0300

    field

--- src/com/company/Harvest.java
+++ src/com/company/Harvest.java
@@ -3,22 +3,24 @@ package com.company;
 public class Harvest {
     public static void main(String[] args) {
 
-        double area = 100;
-        double averageHarvest = 20;
-        int startIndex = 2;
-        int finishIndex = 8;
-        int percentArea = 5;
+        double size = 100;
+        double productivity = 20;
+        int currentYear = 1;
+        int maxYear = 8;
+        int percentSize = 5;
         int percentHarv = 2;
 
-        while (startIndex<=finishIndex) {
 
-            area += (area*percentArea)/100;
-            averageHarvest += (averageHarvest*percentHarv)/100;
-            System.out.println(String.format(("Площадь за %d ый год = %f"), startIndex, area));
+        while (currentYear<maxYear) {
 
-            System.out.println(String.format(("Урожайность за %d ый год = %f"), startIndex, averageHarvest));
+            size += (size * percentSize) / 100;
+            productivity += (productivity * percentHarv) / 100;
 
-            startIndex ++;
+            System.out.println(String.format(("Урожайность за %d ый год = %f"), currentYear, productivity));
+
+            if (currentYear >= 4 && currentYear <= 7) {
+                System.out.println(String.format(("Площадь за %d ый год = %f"), currentYear, size));
+            }
         }
     }
 }
