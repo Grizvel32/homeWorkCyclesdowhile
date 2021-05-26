1ff96650d9a5c6cea7e822a1ffd503003c30d319
Grizvel32 <grizenkov1996@gmail.com>
2021-05-26 07:54:48 +0300

    field

--- src/com/company/Harvest.java
+++ src/com/company/Harvest.java
@@ -7,20 +7,42 @@ public class Harvest {
         double productivity = 20;
         int currentYear = 1;
         int maxYear = 8;
-        int percentSize = 5;
-        int percentHarv = 2;
 
+        double percentSize = 5;
+        double percentProductivity = 2;
 
-        while (currentYear<maxYear) {
+        double crop = size * productivity;
+        double sumCrop = crop;
+        int finishCropYear = 6;
+
+        String outputForProductivity = "";
+        String outputForSize = "";
+        String outputForCrop = "";
+        while (currentYear < maxYear) {
 
             size += (size * percentSize) / 100;
-            productivity += (productivity * percentHarv) / 100;
+            productivity += (productivity * percentProductivity) / 100;
 
-            System.out.println(String.format(("Урожайность за %d ый год = %f"), currentYear, productivity));
+            currentYear++;
+
+            outputForProductivity += String.format("Урожайность за %d ый год = %.3f\n", currentYear, productivity);
 
             if (currentYear >= 4 && currentYear <= 7) {
-                System.out.println(String.format(("Площадь за %d ый год = %f"), currentYear, size));
+             outputForSize +=  String.format("Площадь за %d ый год = %.3f\n", currentYear, size);
             }
+
+            if (currentYear <= finishCropYear) {
+                sumCrop += size * productivity;
+
+
+            }
+
         }
+       outputForCrop += String.format("Урожайность за первые %d лет  = %.3f", finishCropYear, sumCrop);
+
+
+        System.out.println(outputForProductivity);
+        System.out.println(outputForSize);
+        System.out.println(outputForCrop);
     }
 }
